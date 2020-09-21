bbbf28846738b535ff41c7a40c0d4cef6ff8c972
Munir Wanis <munirxavier@wanis.com.br>
2020-09-21 18:38:57 -0300

    Adds tests

--- VaccineExampleTests/VaccineExampleTests.swift
+++ VaccineExampleTests/VaccineExampleTests.swift
@@ -11,24 +11,64 @@ import Vaccine
 
 class VaccineExampleTests: XCTestCase {
 
+    private var serviceMock: ServiceMock!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        serviceMock = ServiceMock()
+        Vaccine.setCure(for: ChuckNorrisServicing.self, with: self.serviceMock)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        serviceMock = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testViewModelSuccess() throws {
+        let response = ChuckNorrisResponse(iconUrl: "", id: "", url: "", value: "My name is Chuck Norris")
+        serviceMock.expectedJoke = response
+        let testExpectation = expectation(description: "Request ends")
+        
+        JokeViewModel().getRandomJoke { result in
+            defer { testExpectation.fulfill() }
+            switch result {
+            case .success(let model):
+                XCTAssertTrue(model.joke == response.value)
+            case .failure(let error):
+                XCTFail("Test failed with error: \(error.localizedDescription)")
+            }
+        }
+        
+        wait(for: [testExpectation], timeout: 10.0)
     }
+    
+    func testViewModelError() throws {
+        serviceMock.expectedError = URLError(.unknown)
+        let testExpectation = expectation(description: "Request ends")
+        
+        JokeViewModel().getRandomJoke { result in
+            defer { testExpectation.fulfill() }
+            switch result {
+            case .success:
+                XCTFail("Test should fail")
+            case .failure(let error):
+                XCTAssertEqual(error as! URLError, URLError(.unknown))
+            }
+        }
+        
+        wait(for: [testExpectation], timeout: 10.0)
+    }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+private class ServiceMock: ChuckNorrisServicing {
+    var expectedJoke: ChuckNorrisResponse?
+    var expectedError: Error?
+    
+    func getRandomJoke(callback: @escaping (Result<ChuckNorrisResponse, Error>) -> Void) {
+        if let joke = expectedJoke {
+            callback(.success(joke))
+        }
+        
+        if let error = expectedError {
+            callback(.failure(error))
         }
     }
-
 }
